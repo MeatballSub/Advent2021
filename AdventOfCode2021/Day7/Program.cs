3647b06ac68222ffdda46dd0f0957e018300b783
Kevin Day <kday@up.com>
2021-12-07 21:16:06 -0600

    Day 7

---
+++ AdventOfCode2021/Day7/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Day7
+{
+    public class Program
+    {
+        static List<int> readInput(string file_name)
+        {
+            List<int> crabs = new List<int>();
+            using(TextReader reader = File.OpenText(file_name))
+            {
+                crabs = reader.ReadLine().Split(',', StringSplitOptions.RemoveEmptyEntries).Select(_ => int.Parse(_)).ToList();
+            }
+            return crabs;
+        }
+
+        static long cost2(long crab, long align)
+        {
+            long n = Math.Abs(crab - align);
+            return (n * (n + 1) / 2);
+        }
+
+        public static long part1(string file_name)
+        {
+            List<int> crabs = readInput(file_name);
+            crabs.Sort();
+            int align = crabs[(crabs.Count + 1) / 2];
+
+            return crabs.Sum(c => Math.Abs(c - align));
+        }
+
+        public static long part2(string file_name)
+        {
+            List<int> crabs = readInput(file_name);
+            int round_down = (int)Math.Floor(crabs.Average());
+            int round_up = (int)Math.Ceiling(crabs.Average());
+
+            long round_down_cost = (long)crabs.Sum(c => cost2(c, round_down));
+            long round_up_cost = (long)crabs.Sum(c => cost2(c, round_up));
+
+            return Math.Min(round_down_cost, round_up_cost);
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(part2("sample_input.txt"));
+            Console.WriteLine(part2("input.txt"));
+        }
+    }
+}
--- AdventOfCode2021/Day7Test/UnitTest1.cs
+++ AdventOfCode2021/Day7Test/UnitTest1.cs
@@ -2,25 +2,25 @@ using System;
 using System.Collections.Generic;
 using Xunit;
 
-namespace Day6Test
+namespace Day7Test
 {
     public class UnitTest1
     {
-        //[Theory]
+        [Theory]
         [MemberData(nameof(Part1sample))]
         [MemberData(nameof(Part1))]
         public void Part1Test(string file_name, int answer)
         {
-            long result = Day6.Program.part1(file_name);
+            long result = Day7.Program.part1(file_name);
             Assert.Equal(answer, result);
         }
 
-        //[Theory]
+        [Theory]
         [MemberData(nameof(Part2sample))]
         [MemberData(nameof(Part2))]
         public void Part2Test(string file_name, long answer)
         {
-            long result = Day6.Program.part2(file_name);
+            long result = Day7.Program.part2(file_name);
             Assert.Equal(answer, result);
         }
 
@@ -32,12 +32,12 @@ namespace Day6Test
             };
         }
 
-        public static IEnumerable<object[]> Part1sample => TestSetup("sample_input.txt", 5934);
+        public static IEnumerable<object[]> Part1sample => TestSetup("sample_input.txt", 37);
 
-        public static IEnumerable<object[]> Part1 => TestSetup("input.txt", 386640);
+        public static IEnumerable<object[]> Part1 => TestSetup("input.txt", 342730);
 
-        public static IEnumerable<object[]> Part2sample => TestSetup("sample_input.txt", 26984457539);
+        public static IEnumerable<object[]> Part2sample => TestSetup("sample_input.txt", 168);
 
-        public static IEnumerable<object[]> Part2 => TestSetup("input.txt", 1733403626279);
+        public static IEnumerable<object[]> Part2 => TestSetup("input.txt", 92335207);
     }
 }
