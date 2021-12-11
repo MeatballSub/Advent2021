0adeec9696bd7d153396769cd3471234a9c1c458
Kevin Day <kday@up.com>
2021-12-11 00:21:51 -0600

    Day 11

---
+++ AdventOfCode2021/Day11/Day11/Program.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+
+namespace Day11
+{
+    public class Program
+    {
+        static HashSet<Point> flash_set;
+        const char FLASH_POINT = (char)('9' + 1);
+
+        static List<char[]> readInput(string file_name)
+        {
+            return File.ReadAllLines(file_name).Select(l => l.ToCharArray()).ToList();
+        }
+
+        static List<char[]> increment(List<char[]> octopi)
+        {
+            return octopi.Select(arr => arr.Select(i => (char)(i + 1)).ToArray()).ToList();
+        }
+
+        static bool boundsCheck(List<char[]> octopi, Point index)
+        {
+            return (index.Y >= 0 && index.Y < octopi.Count && index.X >= 0 && index.X < octopi[index.Y].Length);
+        }
+
+        static List<char[]> flash(List<char[]> octopi, int x, int y)
+        {
+            Point octopus = new Point(x, y);
+            if(flash_set.Add(octopus))
+            {
+                List<Point> neighbors = new List<Point>() { new Point(-1, -1), new Point(-1, 0), new Point(-1, 1), new Point(0, -1), new Point(0, 1), new Point(1, -1), new Point(1, 0), new Point(1, 1) };
+                foreach (Point neighbor in neighbors)
+                {
+                    neighbor.Offset(octopus);
+                    if(boundsCheck(octopi, neighbor))
+                    {
+                        if(++octopi[neighbor.Y][neighbor.X] == FLASH_POINT)
+                        {
+                            flash(octopi, neighbor.X, neighbor.Y);
+                        }
+                    }
+                }
+            }
+            return octopi;
+        }
+
+        static List<char[]> flash(List<char[]> octopi)
+        {
+            for(int y = 0; y < octopi.Count; ++y)
+            {
+                for (int x = 0; x < octopi[y].Length; ++x)
+                {
+                    if (octopi[y][x] >= FLASH_POINT)
+                    {
+                        octopi = flash(octopi, x, y);
+                    }
+                }
+            }
+            return octopi;
+        }
+
+        static List<char[]> reset(List<char[]> octopi)
+        {
+            foreach(Point octopus in flash_set)
+            {
+                octopi[octopus.Y][octopus.X] = '0';
+            }
+            return octopi;
+        }
+
+        static List<char[]> step(List<char[]> octopi)
+        {
+            flash_set.Clear();
+            octopi = increment(octopi);
+            octopi = flash(octopi);
+            octopi = reset(octopi);
+            return octopi;
+        }
+
+        public static long part1(string file_name)
+        {
+            long count = 0;
+            flash_set = new HashSet<Point>();
+
+            var octopi = readInput(file_name);            
+            for (int i = 0; i < 100; ++i)
+            {
+                octopi = step(octopi);
+                count += flash_set.Count;
+            }
+
+            return count;
+        }
+
+        public static long part2(string file_name)
+        {
+            flash_set = new HashSet<Point>();
+
+            var octopi = readInput(file_name);
+            int num_octopi = octopi.Sum(s => s.Length);
+            int i = 0;
+            for (;  flash_set.Count != num_octopi; ++i)
+            {
+                octopi = step(octopi);
+            }
+
+            return i;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(part1("sample_input.txt"));
+            Console.WriteLine(part1("input.txt"));
+            Console.WriteLine(part2("sample_input.txt"));
+            Console.WriteLine(part2("input.txt"));
+        }
+    }
+}
--- AdventOfCode2021/Day11/Day11Test/UnitTest1.cs
+++ AdventOfCode2021/Day11/Day11Test/UnitTest1.cs
@@ -1,25 +1,25 @@
 using System.Collections.Generic;
 using Xunit;
 
-namespace Day10Test
+namespace Day11Test
 {
     public class UnitTest1
     {
-        //[Theory]
+        [Theory]
         [MemberData(nameof(Part1sample))]
         [MemberData(nameof(Part1))]
         public void Part1Test(string file_name, int answer)
         {
-            long result = Day10.Program.part1(file_name);
+            long result = Day11.Program.part1(file_name);
             Assert.Equal(answer, result);
         }
 
-        //[Theory]
+        [Theory]
         [MemberData(nameof(Part2sample))]
         [MemberData(nameof(Part2))]
         public void Part2Test(string file_name, long answer)
         {
-            long result = Day10.Program.part2(file_name);
+            long result = Day11.Program.part2(file_name);
             Assert.Equal(answer, result);
         }
 
@@ -31,12 +31,12 @@ namespace Day10Test
             };
         }
 
-        public static IEnumerable<object[]> Part1sample => TestSetup("sample_input.txt", 26397);
+        public static IEnumerable<object[]> Part1sample => TestSetup("sample_input.txt", 1656);
 
-        public static IEnumerable<object[]> Part1 => TestSetup("input.txt", 358737);
+        public static IEnumerable<object[]> Part1 => TestSetup("input.txt", 1732);
 
-        public static IEnumerable<object[]> Part2sample => TestSetup("sample_input.txt", 288957);
+        public static IEnumerable<object[]> Part2sample => TestSetup("sample_input.txt", 195);
 
-        public static IEnumerable<object[]> Part2 => TestSetup("input.txt", 4329504793);
+        public static IEnumerable<object[]> Part2 => TestSetup("input.txt", 290);
     }
 }
